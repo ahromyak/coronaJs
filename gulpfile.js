bbc8ca842b92e312d4f65d30732b0cf506011055
deepwest83 <deepwest83@mail.ru>
2016-09-03 22:02:17 +0300

    gulp tasks added

--- gulpfile.js
+++ gulpfile.js
@@ -7,14 +7,9 @@ var gulp = require('gulp')
     uglify = require('gulp-uglify'),
     concat = require('gulp-concat');
     sass = require('gulp-sass'),
-    // sourcemaps = require('gulp-sourcemaps'),
-    // rigger = require('gulp-rigger'),
     cssmin = require('gulp-minify-css'),
     imagemin = require('gulp-imagemin'),
     pngquant = require('imagemin-pngquant');
-    // rimraf = require('rimraf'),
-    // browserSync = require("browser-sync");
-
 
 // const lib = [
 //     'node_modules/es6-shim/es6-shim.min.js',
@@ -27,28 +22,26 @@ var gulp = require('gulp')
 //     'node_modules/angular2/bundles/http.dev.js',
 //     'node_modules/angular2/bundles/router.dev.js'
 // ];
-// gulp.task('source-concat', function() {
-//     return gulp.src([
-//             'node_modules/jquery/dist/jquery.min.js',
-//             'node_modules/angular/angular.min.js',
-//             'node_modules/angular-animate/angular-animate.min.js',
-//             'node_modules/angular-aria/angular-aria.min.js',
-//             'node_modules/angular-material/angular-material.min.js',
-//             'src/main/webapp/js/src/moment-with-ru.min.js',
-//             'node_modules/angular-moment/angular-moment.min.js',
-//             'node_modules/twix/dist/twix.min.js',
-//             'src/main/webapp/js/src/lodash.min.js',
-//             'src/main/webapp/js/src/lodash.core.min.js',
-//             'src/main/webapp/js/src/ng-lodash.min.js'
-//         ])
-//         .pipe(newer(tempDir + 'source.min.js'))
-//         .pipe(concat('source.min.js'))
-//         .pipe(gulp.dest(tempDir))
-// });
+
+gulp.task('source-concat', function() {
+    return gulp.src([
+            'node_modules/jquery/dist/jquery.min.js',
+            'node_modules/angular/angular.min.js',
+            'node_modules/angular-animate/angular-animate.min.js',
+            'node_modules/angular-aria/angular-aria.min.js',
+            'node_modules/angular-material/angular-material.min.js',
+            'node_modules/angular-moment/angular-moment.min.js',
+            'assets/js/lodash.min.js',
+            'assets/js/lodash.core.min.js'
+        ])
+        .pipe(concat('lib.js'))
+        .pipe(gulp.dest('app'));
+});
+
 gulp.task('make-js', function () {
-    gulp.src('/app/**/js/*.js')
+    gulp.src('app/**/js/*.js')
         .pipe(concat('app.js'))
-        .pipe(gulp.dest('/app'));
+        .pipe(gulp.dest('app'));
 });
 
 gulp.task('make-css', function () {
